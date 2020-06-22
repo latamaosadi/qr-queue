a62687af8521ea28ee2a2d2aefbb60c23a1b5281
Robert Latamaosadi <robert.latamaosadi@gmail.com>
2020-06-22 17:50:08 +0800

    initial migration

---
+++ database/migrations/2020_06_22_094907_create_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 100);
+            $table->string('nik', 16);
+            $table->string('no_hp', 50);
+            $table->string('email', 50);
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
